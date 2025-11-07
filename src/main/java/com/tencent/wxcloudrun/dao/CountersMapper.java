435291dba50e770b1d17c1ff13294c7c890e779b
youngfreefjs <471011042@qq.com>
2025-11-07 14:39:43 +0800

    init

--- src/main/java/com/tencent/wxcloudrun/dao/CountersMapper.java
+++ src/main/java/com/tencent/wxcloudrun/dao/CountersMapper.java
@@ -1,16 +1,24 @@
 package com.tencent.wxcloudrun.dao;
 
 import com.tencent.wxcloudrun.model.Counter;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.*;
+
 import java.util.List;
 
 @Mapper
 public interface CountersMapper {
 
+  @Select("SELECT id, count FROM counter WHERE id = #{id}")
   Counter getCounter(@Param("id") Integer id);
 
+  @Insert({
+          "<script>",
+          "INSERT INTO counter (id, count) VALUES (#{id}, #{count})",
+          "ON DUPLICATE KEY UPDATE count = #{count}",
+          "</script>"
+  })
   void upsertCount(Counter counter);
 
+  @Delete("DELETE FROM counter WHERE id = #{id}")
   void clearCount(@Param("id") Integer id);
 }
