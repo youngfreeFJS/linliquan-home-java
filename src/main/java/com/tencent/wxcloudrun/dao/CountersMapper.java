fca6b1f15195592640ef50194f9cb8c14fd6354e
youngfreefjs <471011042@qq.com>
2025-11-10 14:57:05 +0800

    init

--- src/main/java/com/tencent/wxcloudrun/dao/CountersMapper.java
+++ src/main/java/com/tencent/wxcloudrun/dao/CountersMapper.java
@@ -8,17 +8,17 @@ import java.util.List;
 @Mapper
 public interface CountersMapper {
 
-  @Select("SELECT id, count FROM counter WHERE id = #{id}")
+  @Select("SELECT id, count FROM Counters WHERE id = #{id}")
   Counter getCounter(@Param("id") Integer id);
 
   @Insert({
           "<script>",
-          "INSERT INTO counter (id, count) VALUES (#{id}, #{count})",
+          "INSERT INTO Counters (id, count) VALUES (#{id}, #{count})",
           "ON DUPLICATE KEY UPDATE count = #{count}",
           "</script>"
   })
   void upsertCount(Counter counter);
 
-  @Delete("DELETE FROM counter WHERE id = #{id}")
+  @Delete("DELETE FROM Counters WHERE id = #{id}")
   void clearCount(@Param("id") Integer id);
 }
